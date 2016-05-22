d41c9347b52e28d20b8a676c1bb318063d0f426a
Alex686 <matvey0793@bk.ru>
2016-05-22 16:18:38 +0300

    Revert "added saiku classes and new library in gradle file"

--- app/src/main/java/com/alex/test/saiku/olap/query2/util/olap/query2/dto/olap/query2/Parameterizable.java
+++
@@ -1,8 +0,0 @@
-package com.alex.test.saiku.olap.query2.util.olap.query2.dto.olap.query2;
-
-import java.util.List;
-
-public interface Parameterizable {
-	
-	List<String> getParameterNames();
-}
--- app/src/main/java/com/alex/test/saiku/olap/query2/util/olap/query2/dto/service/util/ISaikuQuery.java
+++
@@ -1,5 +0,0 @@
-package com.alex.test.saiku.olap.query2.util.olap.query2.dto.service.util;
-
-public interface ISaikuQuery {
-
-}
